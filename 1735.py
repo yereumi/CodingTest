88fc6c089970f5d325dc666e75709ecf9bda8b24
yereumi <lovelyyeji01@gmail.com>
2024-01-14 01:13:51 +0900

    시간초과

--- 1735.py
+++ 1735.py
@@ -5,13 +5,13 @@ mom = b1*b2
 son = a1*b2 + a2*b1
 
 if mom < son:
-    min_num = mom
+    min_num = int(mom/2) + 1
 else:
-    min_num = son
+    min_num = int(son/2) + 1
 
 for i in range(1, min_num + 1):
     if mom%i==0 and son%i==0:
         mom /= i
         son /= i
 
-print(son, mom)
+print(int(son), int(mom))
