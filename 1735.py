2467600907fa537c0a1c58adcf43cf7776c681ef
yereumi <lovelyyeji01@gmail.com>
2024-01-14 00:16:26 +0900

    1735

--- 1735.py
+++ 1735.py
@@ -4,4 +4,14 @@ a2, b2 = map(int, input().split())
 mom = b1*b2
 son = a1*b2 + a2*b1
 
+if mom < son:
+    min_num = mom
+else:
+    min_num = son
+
+for i in range(1, min_num + 1):
+    if mom%i==0 and son%i==0:
+        mom /= i
+        son /= i
+
 print(son, mom)
